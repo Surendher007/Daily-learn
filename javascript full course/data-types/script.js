35a22e8561a9f1f36124e166e685ccd140fb6ff0
Suren007 <surendherbabu13@gmail.com>
2024-07-09 05:55:34 +0530

    update

---
+++ javascript full course/data-types/script.js
@@ -0,0 +1,66 @@
+// javascript introduction
+
+/*Data types
+
+// There are three data type
+
+// 1.var
+   *var is global scope
+   *reclare the value & reassign the value
+   *
+
+// 2.let
+   *let is a local scope
+   *only for reassign the value not for redeclare
+
+// 3.const
+   *const is never changed
+   *not for reassign and reclare
+
+*/
+
+//1. var
+var a = 10
+console.log(a)
+
+// reassign
+
+a = 20
+console.log(a)
+
+// redeclare
+
+var a = 23
+console.log(a)
+
+// 2.  let
+
+let b = 1
+console.log(b)
+
+// reassign
+
+b = 2
+console.log(b)
+
+// reclare
+
+// let b = 3
+// console.log(b)
+// this part throe the identifier error b has been already declared
+
+// 3. const
+
+const c = 24
+console.log(c)
+
+// reclare & reassign
+
+// c = 25
+// console.log(c)
+// assignment to const variable
+
+// const c = 17
+// console.log(c)
+// this part throe the identifier error b has been already declared
+
